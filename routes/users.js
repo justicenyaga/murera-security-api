542dc67ef059bca23b6d68293ef68e950bbde9ea
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-04 13:38:14 +0300

    Create a route to register users

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
+
+const { User, validateUser } = require("../models/user");
+const validateWith = require("../middlewares/validate");
+
+router.post("/", validateWith(validateUser), async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(
+    _.pick(req.body, ["firstName", "lastName", "email", "password"]),
+  );
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  res.send(_.pick(user, ["_id", "firstName", "lastName", "email"]));
+});
+
+module.exports = router;
