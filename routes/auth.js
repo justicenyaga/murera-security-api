912db852837ac0aa2f771d31c50d09a938ec75ff
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-04 15:13:46 +0300

    Create a route to authenticate users

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const express = require("express");
+const router = express.Router();
+
+const { User } = require("../models/user");
+const validateWith = require("../middlewares/validate");
+
+router.post("/", validateWith(validateBody), async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validpassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validpassword) return res.status(400).send("Invalid email or password.");
+
+  const token = jwt.sign(
+    { _id: user._id, isAdmin: user.isAdmin, isSuperAdmin: user.isSuperAdmin },
+    config.get("jwtPrivateKey"),
+  );
+  res.send(token);
+});
+
+function validateBody(body) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+  return schema.validate(body);
+}
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
+const auth = require("../routes/auth");
 const users = require("../routes/users");
 const error = require("../middlewares/error");
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use("/api/auth", auth);
   app.use("/api/users", users);
   app.use(error);
 };
