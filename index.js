897de5d0d3c2ae87ac95ef55dd164af443cfd1a0
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-04 13:37:43 +0300

    Encapsulate project routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const logger = require("./logger");
 
 require("./startup/db")();
+require("./startup/routes")(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
