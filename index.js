5d3659da3d93aae6b817743e947bc8e2f97797ed
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-04 15:14:59 +0300

    Ensure jwtPrivateKey environment variable is set

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const logger = require("./logger");
 
+require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
 
