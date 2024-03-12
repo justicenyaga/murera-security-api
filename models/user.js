ee26cf64264d4ea21be0ebf1e0fd3e8e1d002583
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-12 18:52:04 +0300

    Add timestamps on all models

--- models/user.js
+++ models/user.js
@@ -3,39 +3,42 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: Boolean,
+    isSuperAdmin: Boolean,
   },
-  lastName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 1024,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  isAdmin: Boolean,
-  isSuperAdmin: Boolean,
-});
+  { timestamps: true },
+);
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
