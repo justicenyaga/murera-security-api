2a702c9397662eecb77afe46ed77059cfd4f1530
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-15 16:04:36 +0300

    Ensure email credentials are set as env variables

--- startup/config.js
+++ startup/config.js
@@ -4,4 +4,10 @@ module.exports = function () {
   if (!config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
+  if (!config.get("email")) {
+    throw new Error("FATAL ERROR: email is not defined.");
+  }
+  if (!config.get("emailPassword")) {
+    throw new Error("FATAL ERROR: emailPassword is not defined.");
+  }
 };
