3c1ddccb8c506155fa14505b5cd6fad0c832fee5
Justice Nyaga <justiceevansmwema@gmail.com>
2024-04-02 14:14:30 +0300

    Change phone's max length to 15

--- models/resident.js
+++ models/resident.js
@@ -56,7 +56,7 @@ const residentSchema = new mongoose.Schema(
       type: String,
       required: true,
       minlength: 6,
-      maxlength: 20,
+      maxlength: 15,
     },
     gender: {
       type: String,
@@ -97,7 +97,7 @@ const Resident = mongoose.model("Resident", residentSchema);
 function validateResident(resident) {
   const schema = Joi.object({
     dob: Joi.date().required(),
-    phone: Joi.string().min(6).max(20).required(),
+    phone: Joi.string().min(6).max(15).required(),
     gender: Joi.string().min(2).max(30).required(),
     county: Joi.string().min(3).max(50).required(),
     subCounty: Joi.string().min(3).max(50).required(),
