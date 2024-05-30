0df6b7505a72d80f135fc30c03d9e9d8dd2e229b
Justice Nyaga <justiceevansmwema@gmail.com>
2024-05-30 14:25:48 +0300

    Mask user's national ID

--- mappers/user.js
+++ mappers/user.js
@@ -2,8 +2,15 @@ const config = require("config");
 
 const mapper = (user) => {
   const baseUrl = config.get("assetsBaseUrl");
+
+  let nationalId = user.nationalId.toString();
+  const lastThreeDigits = nationalId.slice(-3);
+  const asterisks = "*".repeat(nationalId.length - 3);
+  nationalId = `${asterisks}${lastThreeDigits}`;
+
   return {
     ...user,
+    nationalId,
     image: baseUrl + user.image,
   };
 };
