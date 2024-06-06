1cf2b94f7f4c21982bf7cd1b75b84da629813ec7
Justice Nyaga <justiceevansmwema@gmail.com>
2024-06-06 17:39:38 +0300

    Update station schema

--- models/station.js
+++ models/station.js
@@ -1,63 +1,47 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const locationSchema = new mongoose.Schema(
-  {
-    longitude: {
-      type: Number,
-      required: true,
+const PoliceStation = mongoose.model(
+  "PoliceStation",
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 15,
+        unique: true,
+      },
+      subCounty: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubCounty",
+        required: true,
+      },
+      officersCount: {
+        type: Number,
+        default: 0,
+      },
     },
-    latitude: {
-      type: Number,
-      required: true,
-    },
-  },
-  { _id: false },
+    { timestamps: true },
+  ),
 );
-
-const stationSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    location: {
-      type: locationSchema,
-      required: true,
-    },
-    officersCount: {
-      type: Number,
-      default: 0,
-    },
-  },
-  { timestamps: true },
-);
-
-const PoliceStation = mongoose.model("PoliceStation", stationSchema);
 
 function validateStation(station) {
   const schema = Joi.object({
-    name: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
-    address: Joi.string().required(),
+    name: Joi.string().min(3).max(255).required(),
+    phone: Joi.string().min(6).max(15).required(),
+    subCounty: Joi.objectId().required(),
     officersCount: Joi.number(),
-    location: Joi.object({
-      longitude: Joi.number().required(),
-      latitude: Joi.number().required(),
-    }).required(),
   });
 
   return schema.validate(station);
 }
 
-exports.PoliceStation = PoliceStation;
-exports.stationSchema = stationSchema;
 exports.validateStation = validateStation;
+exports.PoliceStation = PoliceStation;
